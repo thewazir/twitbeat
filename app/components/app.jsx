01aff7b28742b63f485b57c590c4d8152f003a20
Clayton Ferris <cferris@gmail.com>
2015-07-18 15:48:13 -0700

    move socket connection and tweets object to the app component and flow down via props

--- app/components/app.jsx
+++ app/components/app.jsx
@@ -1,11 +1,34 @@
 import React from "react/addons"
+import {Howl} from "howler"
 import TweetLineContainer from "./tweetlinecontainer"
 
 
 export default class App extends React.Component {
+    constructor( props ) {
+        super(props);
+        this.state = {
+            tweets: []
+        }
+        this.sound = new Howl({
+            urls: ['/dist/sounds/ECGBeepSound.mp3']
+        });
+    }
+
     render() {
         return (<div>
-            <TweetLineContainer/>
+            <TweetLineContainer tweets={this.state.tweets} />
         </div>);
+    }
+
+    componentWillMount() {
+        this.socket = io.connect();
+        this.socket.on('tweet', data => this.newTweet(data));
+    }
+
+    newTweet( data ) {
+        let tweets = this.state.tweets;
+        this.sound.play();
+        tweets.push(data);
+        this.setState({tweets});
     }
 }
--- app/components/tweetlinecontainer.jsx
+++ app/components/tweetlinecontainer.jsx
@@ -1,34 +1,11 @@
 import React from "react/addons"
-import {Howl} from "howler"
 import TweetLine from "./tweetline"
 
 console.log(TweetLine);
 export default class TweetLineContainer extends React.Component {
-    constructor( props ) {
-        super(props);
-        this.state = {
-            tweets: []
-        }
-        this.sound = new Howl({
-            urls: ['/dist/sounds/ECGBeepSound.mp3']
-        });
-    }
-
-    componentWillMount() {
-        this.socket = io.connect();
-        this.socket.on('tweet', data => this.newTweet(data));
-    }
-
-    newTweet( data ) {
-        let tweets = this.state.tweets;
-        this.sound.play();
-        tweets.push(data);
-        this.setState({tweets});
-    }
-
     render() {
         return (<div>
-            <TweetLine tweets={this.state.tweets}/>
+            <TweetLine {...this.props} />
         </div>);
     }
 }
