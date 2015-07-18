27a757f27f44853da0f047b674fbcf04d0426ec3
Charlie <bringking@gmail.com>
2015-07-18 16:40:20 -0700

    updated styling

--- app/components/tweets/tweetList.jsx
+++ app/components/tweets/tweetList.jsx
@@ -9,7 +9,7 @@ export default  class TweetList extends React.Component {
 
         let tweets = this.props.tweets.reverse().map(( t, idx ) => {
             return <li key={idx}>
-                <a className="tweetLink" href={t.url}><h4>{t.name}</h4><p>{t.text}</p><p>{t.points}</p></a>
+                <a className="tweetLink" href={t.url}><h4>{t.name}</h4><p>{t.text}</p><p className="cactus-points">Cactus Points: {t.points}</p></a>
             </li>
         });
         console.log(this.props.tweets);
