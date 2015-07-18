f1dad165d4cf4bfb8e188270459ca1b70395476e
Omer Wazir <omer.wazir@gmail.com>
2015-07-18 16:18:01 -0700

    Tweets on the page

--- app/components/tweets/tweetList.jsx
+++ app/components/tweets/tweetList.jsx
@@ -1,18 +1,27 @@
 import React from "react/addons"
 
-class TweetList extends React.Component {
+export default  class TweetList extends React.Component {
     constructor( props ) {
         super(props);
     }
 
     render(){
+
+        let tweets = this.props.tweets.map((t, idx) => {
+            return <li key={idx}>
+            <a className="tweetLink" href={t.url}>{t.name}:{t.text}-{t.points}</a>
+            </li>
+        });
+        console.log(this.props.tweets);
+
+       return (
+        
         <div className="text-container">
-            <h1>text-container</h1>
-        </div>
+            <h1>Tweets</h1>
+            <ul className="tweetList">
+                {tweets}
+            </ul>
+        </div>)
     }
 
 }
-
-
-TweetLine.propTypes = {tweets: React.PropTypes.array};
-export default TweetList;
