06ee55c490c9c0fb48e607f9a715152ec7c34df6
Omer Wazir <omer.wazir@gmail.com>
2015-07-18 16:17:29 -0700

    Tweets on the page

--- app/components/tweetlinecontainer.jsx
+++ app/components/tweetlinecontainer.jsx
@@ -1,11 +1,14 @@
 import React from "react/addons"
 import TweetLine from "./tweetline"
+import TweetList from "./tweets/tweetList"
 import {getRandomInt} from "../utils"
 
 export default class TweetLineContainer extends React.Component {
     render() {
-        return (<div>
+        return (
+        <div>
             <TweetLine {...this.props} />
+            <TweetList {...this.props} />
         </div>);
     }
 }
