b4946cafffcd2d22844f1362c5b6d5602a01c6d2
Charlie <bringking@gmail.com>
2015-07-18 13:13:56 -0700

    added app component

--- app/client.js
+++ app/client.js
@@ -1,3 +1,22 @@
 //
 // Created by charlesking on 7/18/15.
 //
+/**
+ * The client JS file that brings in React and bootstraps the app into the page from the server state.
+ */
+import React from "react"
+import { Router, Route, Link, Redirect } from 'react-router'
+import { history } from 'react-router/lib/BrowserHistory'
+
+//components
+import App from "./app"
+
+
+//put React into the global scope for chrome dev tool support
+window.React = React;
+
+//grab the app node
+const mountNode = document.getElementById('app');
+
+//render our app component into that node
+React.render(<App/>, mountNode);
